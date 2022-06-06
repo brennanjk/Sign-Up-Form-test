a94505d29d1a2cb333d9b7bf18c3630dedf9424c
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-06-06 13:45:20 -0700

    Add validation so phone number field only accepts numbers; add password requirements and border coloring effects for when passwords match or don't match

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ function check_pass() {
     if (document.getElementById('password').value === document.getElementById('confirmPassword').value) {
         document.getElementById('message').style.color = 'green';
         document.getElementById('message').textContent = 'Passwords match';
+        document.getElementById('password').style.borderColor = 'green'
+        document.getElementById('confirmPassword').style.borderColor = 'green'
     } else {
         document.getElementById('message').style.color = 'red';
         document.getElementById('message').textContent = '*Passwords do not match';
+        document.getElementById('password').style.borderColor = 'red'
+        document.getElementById('confirmPassword').style.borderColor = 'red'
     }
 }
