02295c888b9428430f67680623b64b600d950929
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-06-06 14:09:36 -0700

    Adjust password message colors to better fit the new background color

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ function check_pass() {
     if (document.getElementById('password').value === document.getElementById('confirmPassword').value) {
         document.getElementById('message').style.color = 'green';
         document.getElementById('message').textContent = 'Passwords match';
-        document.getElementById('password').style.borderColor = 'green'
+        document.getElementById('password').style.borderColor = 'rgb(0,255, 13)'
         document.getElementById('confirmPassword').style.borderColor = 'green'
     } else {
-        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').style.color = 'rgb(155,0,0)';
         document.getElementById('message').textContent = '*Passwords do not match';
         document.getElementById('password').style.borderColor = 'red'
         document.getElementById('confirmPassword').style.borderColor = 'red'
