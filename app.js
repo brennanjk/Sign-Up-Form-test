2c98da56041fdbe1fe49b2ce81d754320ebd0c84
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-06-06 17:17:22 -0700

    Adjust color on password match message; add * before Password in that message

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function check_pass() {
     if (document.getElementById('password').value === document.getElementById('confirmPassword').value) {
-        document.getElementById('message').style.color = 'green';
-        document.getElementById('message').textContent = 'Passwords match';
+        document.getElementById('message').style.color = 'rgb(252,249,146)';
+        document.getElementById('message').textContent = '*Passwords match';
         document.getElementById('password').style.borderColor = 'rgb(0,255, 13)'
         document.getElementById('confirmPassword').style.borderColor = 'green'
     } else {
